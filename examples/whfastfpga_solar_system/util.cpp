e652d450e78b35e34ea81a0dfe52c3dfe251daf4
Gus Beane <angus.beane@cfa.harvard.edu>
2025-05-05 11:11:09 -0400

    Fix COM shift

--- examples/whfastfpga_solar_system/util.cpp
+++ examples/whfastfpga_solar_system/util.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <cmath>
+#include <cstdio>
 #include "util.h"
 
 // Moves all bodies so that the system's center of mass is at the origin
@@ -30,18 +31,20 @@ void move_to_center_of_mass(std::array<Body, N_BODIES>& bodies) {
 // Transforms positions and velocities from barycentric inertial to democratic heliocentric coordinates
 void inertial_to_democraticheliocentric_posvel(std::array<Body, N_BODIES>& bodies) {
     // Assume bodies[0] is the central star
-    double m0 = bodies[0].mass;
 
     // Shift planets to heliocentric coordinates (relative to star)
     for (std::size_t i = 1; i < N_BODIES; ++i) {
+        
         for (int j = 0; j < 3; ++j) {
             bodies[i].pos[j] -= bodies[0].pos[j];
         }
     }
     
+    double m0 = 0.0;
     // Compute center of mass velocity of solar system
     std::array<double, 3> com_vel = {0.0, 0.0, 0.0};
-    for (std::size_t i = 1; i < N_BODIES; ++i) {
+    for (std::size_t i = 0; i < N_BODIES; ++i) {
+        m0 += bodies[i].mass;
         for (int j = 0; j < 3; ++j) {
             com_vel[j] += bodies[i].mass * bodies[i].vel[j];
         }
@@ -56,5 +59,31 @@ void inertial_to_democraticheliocentric_posvel(std::array<Body, N_BODIES>& bodie
             bodies[i].vel[j] -= com_vel[j];
         }
     }
+
+//     // Debug: print body 0 and body 7 positions and velocities
+//     printf("particle 1 pos=(%e, %e, %e) vel=(%e, %e, %e)\n",
+//             bodies[1].pos[0], bodies[1].pos[1], bodies[1].pos[2],
+//             bodies[1].vel[0], bodies[1].vel[1], bodies[1].vel[2]);
+    
+//     printf("particle 8 pos=(%e, %e, %e) vel=(%e, %e, %e)\n",
+//             bodies[8].pos[0], bodies[8].pos[1], bodies[8].pos[2],
+//             bodies[8].vel[0], bodies[8].vel[1], bodies[8].vel[2]);
+
+
+// x[1] = -0.050606442387163809
+// y[1] = -0.46199153044180374
+// z[1] = -0.033112314887493777
+// vx[1] = 1.2978664296086504
+// vy[1] = -0.095245414385742383
+// vz[1] = -0.1267757435000321
+// x[8] = 29.796803635571621
+// y[8] = -2.5135110786022508
+// z[8] = -0.63492520913961792
+// vx[8] = 0.014142948749760034
+// vy[8] = 0.18292110904074921
+// vz[8] = -0.0040928456197298641
+
+
+
 }
 

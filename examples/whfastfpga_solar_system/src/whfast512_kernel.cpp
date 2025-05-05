602acac7635777ffe5b05332c480aae2e5d3c0bb
Angus Beane <angus.beane@cfa.harvard.edu>
2025-05-05 18:48:19 -0400

    Add interaction step

--- examples/whfastfpga_solar_system/src/whfast512_kernel.cpp
+++ examples/whfastfpga_solar_system/src/whfast512_kernel.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "whfast512_kernel.h"
 #include "whfast512_constants.h"
 
@@ -43,7 +44,7 @@ static void inline mm_stiefel_Gs03_avx512(__m512d * Gs0, __m512d * Gs1, __m512d 
 };
 
 // Newton step function for Kepler iteration
-static void inline newton_step(__m512d* X, __m512d beta, __m512d r0, __m512d eta0, __m512d zeta0) {
+static void inline newton_step(__m512d* X, __m512d beta, __m512d r0, __m512d eta0, __m512d zeta0, __m512d _dt) {
     __m512d Gs1, Gs2, Gs3;
     mm_stiefel_Gs13_avx512(&Gs1, &Gs2, &Gs3, beta, *X);
     
@@ -56,7 +57,7 @@ static void inline newton_step(__m512d* X, __m512d beta, __m512d r0, __m512d eta
     *X = _mm512_mul_pd(*X, eta0Gs1zeta0Gs2);
     *X = _mm512_fnmadd_pd(eta0, Gs2, *X);
     *X = _mm512_fnmadd_pd(zeta0, Gs3, *X);
-    *X = _mm512_add_pd(_mm512_set1_pd(1.0), *X); // Using dt parameter
+    *X = _mm512_add_pd(_dt, *X); // Using dt parameter
     *X = _mm512_mul_pd(ri, *X);
 }
 
@@ -129,7 +130,7 @@ void whfast512_kepler_step(__m512d *x_vec,  __m512d *y_vec,  __m512d *z_vec,
         // Iterations
         halley_step(&X, beta, r0, eta0, zeta0, _dt);
         halley_step(&X, beta, r0, eta0, zeta0, _dt);
-        newton_step(&X, beta, r0, eta0, zeta0);
+        newton_step(&X, beta, r0, eta0, zeta0, _dt);
         
         // Final Newton step (note: X not needed after this) 
         mm_stiefel_Gs13_avx512(&Gs1, &Gs2, &Gs3, beta, X);
@@ -141,16 +142,24 @@ void whfast512_kepler_step(__m512d *x_vec,  __m512d *y_vec,  __m512d *z_vec,
         // f and g function
     
         __m512d nf = _mm512_mul_pd(kConsts->_M,Gs2); //negative f
-        nf = _mm512_mul_pd(nf,r0i); 
+        nf = _mm512_mul_pd(nf,r0i);
+
+        printf("nf[0] = %f\n", nf[0]);
     
         __m512d g = _mm512_fnmadd_pd(kConsts->_M, Gs3, _dt);
+
+        printf("g[0] = %f\n", g[0]);
     
         __m512d nfd = _mm512_mul_pd(kConsts->_M, Gs1); // negative fd
         nfd = _mm512_mul_pd(nfd, r0i);
         nfd = _mm512_mul_pd(nfd, ri);
+
+        printf("nfd[0] = %f\n", nfd[0]);
     
         __m512d ngd = _mm512_mul_pd(kConsts->_M, Gs2); // negative gd
         ngd = _mm512_mul_pd(ngd, ri);
+
+        printf("ngd[0] = %f\n", ngd[0]);
     
         __m512d nx = _mm512_fnmadd_pd(nf, *x_vec, *x_vec);
         nx = _mm512_fmadd_pd(g, *vx_vec, nx);
@@ -182,8 +191,11 @@ void whfast512_drift_step(__m512d *x_vec,  __m512d *y_vec,  __m512d *z_vec,
     __m512d m_vec, Body *com, double dt)
 {
 // We first do the kepler step, then the com step.
-    whfast512_kepler_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt);
-    whfast512_com_step(com, dt);
+__m512d xload = _mm512_loadu_pd(x_vec);
+
+whfast512_kepler_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt);
+
+whfast512_com_step(com, dt);
 }
 
 // void whfast512_jump_step(__m512d *x_vec,  __m512d *y_vec,  __m512d *z_vec,
@@ -221,12 +233,204 @@ void whfast512_jump_step(__m512d *x_vec, __m512d *y_vec, __m512d *z_vec,
     *z_vec = _mm512_fmadd_pd(sumz, pf512, *z_vec);
 }
 
-// void whfast512_interaction_step(__m512d *x_vec,  __m512d *y_vec,  __m512d *z_vec,
-//     __m512d *vx_vec, __m512d *vy_vec, __m512d *vz_vec,
-//     __m512d m_vec, double dt)
-// {
+// Helper functions for the interaction step
+static __m512d inline gravity_prefactor_avx512_one(__m512d dx, __m512d dy, __m512d dz)
+{
+    __m512d r2 = _mm512_mul_pd(dx, dx);
+    r2 = _mm512_fmadd_pd(dy, dy, r2);
+    r2 = _mm512_fmadd_pd(dz, dz, r2);
+    const __m512d r = _mm512_sqrt_pd(r2);
+    const __m512d r3 = _mm512_mul_pd(r, r2);
+    return _mm512_div_pd(kConsts->one, r3);
+}
 
-// }
+static __m512d inline gravity_prefactor_avx512(__m512d m, __m512d dx, __m512d dy, __m512d dz)
+{
+    __m512d r2 = _mm512_mul_pd(dx, dx);
+    r2 = _mm512_fmadd_pd(dy, dy, r2);
+    r2 = _mm512_fmadd_pd(dz, dz, r2);
+    const __m512d r = _mm512_sqrt_pd(r2);
+    const __m512d r3 = _mm512_mul_pd(r, r2);
+    return _mm512_div_pd(m, r3);
+}
+
+// Performs one full interaction step
+void whfast512_interaction_step(__m512d *x_vec, __m512d *y_vec, __m512d *z_vec,
+                                __m512d *vx_vec, __m512d *vy_vec, __m512d *vz_vec,
+                                __m512d m_vec, double dt)
+{
+    __m512d x_j = *x_vec;
+    __m512d y_j = *y_vec;
+    __m512d z_j = *z_vec;
+    __m512d dt512 = _mm512_set1_pd(dt);
+
+    // General relativistic corrections
+    if (true)
+    {
+        __m512d r2 = _mm512_mul_pd(x_j, x_j);
+        r2 = _mm512_fmadd_pd(y_j, y_j, r2);
+        r2 = _mm512_fmadd_pd(z_j, z_j, r2);
+        const __m512d r4 = _mm512_mul_pd(r2, r2);
+        __m512d prefac = _mm512_div_pd(kConsts->gr_prefac, r4);
+        prefac = _mm512_mul_pd(prefac, dt512);
+        __m512d dvx = _mm512_mul_pd(prefac, x_j);
+        __m512d dvy = _mm512_mul_pd(prefac, y_j);
+        __m512d dvz = _mm512_mul_pd(prefac, z_j);
+        *vx_vec = _mm512_sub_pd(*vx_vec, dvx);
+        *vy_vec = _mm512_sub_pd(*vy_vec, dvy);
+        *vz_vec = _mm512_sub_pd(*vz_vec, dvz);
+
+        // Calculate back reaction onto star and apply them to planets (heliocentric)
+        dvx = _mm512_mul_pd(kConsts->gr_prefac2, dvx);
+        dvy = _mm512_mul_pd(kConsts->gr_prefac2, dvy);
+        dvz = _mm512_mul_pd(kConsts->gr_prefac2, dvz);
+
+        dvx = _mm512_add_pd(_mm512_shuffle_pd(dvx, dvx, 0x55), dvx); // Swapping neighbouring elements
+        dvx = _mm512_add_pd(_mm512_permutex_pd(dvx, _MM_PERM_ABCD), dvx);
+        dvx = _mm512_add_pd(_mm512_shuffle_f64x2(dvx, dvx, 78), dvx);
+        dvy = _mm512_add_pd(_mm512_shuffle_pd(dvy, dvy, 0x55), dvy);
+        dvy = _mm512_add_pd(_mm512_permutex_pd(dvy, _MM_PERM_ABCD), dvy);
+        dvy = _mm512_add_pd(_mm512_shuffle_f64x2(dvy, dvy, 78), dvy);
+        dvz = _mm512_add_pd(_mm512_shuffle_pd(dvz, dvz, 0x55), dvz);
+        dvz = _mm512_add_pd(_mm512_permutex_pd(dvz, _MM_PERM_ABCD), dvz);
+        dvz = _mm512_add_pd(_mm512_shuffle_f64x2(dvz, dvz, 78), dvz);
+
+        *vx_vec = _mm512_sub_pd(*vx_vec, dvx);
+        *vy_vec = _mm512_sub_pd(*vy_vec, dvy);
+        *vz_vec = _mm512_sub_pd(*vz_vec, dvz);
+    }
+
+    __m512d m_j = _mm512_mul_pd(m_vec, dt512);
+    __m512d m_j_01234567 = m_j;
+
+    {
+        x_j = _mm512_permutex_pd(x_j, _MM_PERM_BACD); // within 256
+        y_j = _mm512_permutex_pd(y_j, _MM_PERM_BACD);
+        z_j = _mm512_permutex_pd(z_j, _MM_PERM_BACD);
+        m_j = _mm512_permutex_pd(m_j, _MM_PERM_BACD);
+        __m512d dx_j = _mm512_sub_pd(*x_vec, x_j);
+        __m512d dy_j = _mm512_sub_pd(*y_vec, y_j);
+        __m512d dz_j = _mm512_sub_pd(*z_vec, z_j);
+        __m512d prefact = gravity_prefactor_avx512_one(dx_j, dy_j, dz_j);
+
+        // 0123 4567
+        // 3201 7645
+        __m512d prefact1 = _mm512_mul_pd(prefact, m_j);
+        *vx_vec = _mm512_fnmadd_pd(prefact1, dx_j, *vx_vec);
+        *vy_vec = _mm512_fnmadd_pd(prefact1, dy_j, *vy_vec);
+        *vz_vec = _mm512_fnmadd_pd(prefact1, dz_j, *vz_vec);
+
+        dx_j = _mm512_permutex_pd(dx_j, _MM_PERM_ABDC); // within 256
+        dy_j = _mm512_permutex_pd(dy_j, _MM_PERM_ABDC);
+        dz_j = _mm512_permutex_pd(dz_j, _MM_PERM_ABDC);
+        prefact = _mm512_permutex_pd(prefact, _MM_PERM_ABDC);
+        m_j = _mm512_permute_pd(m_j, 0x55); // within 128
+
+        // 0123 4567
+        // 2310 6754
+        __m512d prefact2 = _mm512_mul_pd(prefact, m_j);
+        *vx_vec = _mm512_fmadd_pd(prefact2, dx_j, *vx_vec);
+        *vy_vec = _mm512_fmadd_pd(prefact2, dy_j, *vy_vec);
+        *vz_vec = _mm512_fmadd_pd(prefact2, dz_j, *vz_vec);
+    }
+    {
+        x_j = _mm512_permutex_pd(x_j, _MM_PERM_BACD); // within 256
+        y_j = _mm512_permutex_pd(y_j, _MM_PERM_BACD);
+        z_j = _mm512_permutex_pd(z_j, _MM_PERM_BACD);
+        m_j = _mm512_permutex_pd(m_j, _MM_PERM_ABDC);
+
+        const __m512d dx_j = _mm512_sub_pd(*x_vec, x_j);
+        const __m512d dy_j = _mm512_sub_pd(*y_vec, y_j);
+        const __m512d dz_j = _mm512_sub_pd(*z_vec, z_j);
+
+        // 0123 4567
+        // 1032 5476
+        const __m512d prefact = gravity_prefactor_avx512(m_j, dx_j, dy_j, dz_j);
+        *vx_vec = _mm512_fnmadd_pd(prefact, dx_j, *vx_vec);
+        *vy_vec = _mm512_fnmadd_pd(prefact, dy_j, *vy_vec);
+        *vz_vec = _mm512_fnmadd_pd(prefact, dz_j, *vz_vec);
+    }
+
+    // //////////////////////////////////////
+    // 256 bit lane crossing
+    // //////////////////////////////////////
+
+    __m512d dvx; // delta vx for 4567 1230
+    __m512d dvy;
+    __m512d dvz;
+
+    {
+        x_j = _mm512_permutexvar_pd(kConsts->so1, x_j); // accros 512
+        y_j = _mm512_permutexvar_pd(kConsts->so1, y_j);
+        z_j = _mm512_permutexvar_pd(kConsts->so1, z_j);
+        m_j = _mm512_permutexvar_pd(kConsts->so1, m_j);
+
+        __m512d dx_j = _mm512_sub_pd(*x_vec, x_j);
+        __m512d dy_j = _mm512_sub_pd(*y_vec, y_j);
+        __m512d dz_j = _mm512_sub_pd(*z_vec, z_j);
+        __m512d prefact = gravity_prefactor_avx512_one(dx_j, dy_j, dz_j);
+
+        // 0123 4567
+        // 4567 1230
+        __m512d prefact1 = _mm512_mul_pd(prefact, m_j);
+        *vx_vec = _mm512_fnmadd_pd(prefact1, dx_j, *vx_vec);
+        *vy_vec = _mm512_fnmadd_pd(prefact1, dy_j, *vy_vec);
+        *vz_vec = _mm512_fnmadd_pd(prefact1, dz_j, *vz_vec);
+
+        // 4567 1230
+        // 0123 4567
+        prefact = _mm512_mul_pd(prefact, m_j_01234567);
+        dvx = _mm512_mul_pd(prefact, dx_j);
+        dvy = _mm512_mul_pd(prefact, dy_j);
+        dvz = _mm512_mul_pd(prefact, dz_j);
+    }
+
+    {
+        x_j = _mm512_permutex_pd(x_j, _MM_PERM_ADCB); // within 256
+        y_j = _mm512_permutex_pd(y_j, _MM_PERM_ADCB);
+        z_j = _mm512_permutex_pd(z_j, _MM_PERM_ADCB);
+        m_j = _mm512_permutex_pd(m_j, _MM_PERM_ADCB);
+
+        __m512d dx_j = _mm512_sub_pd(*x_vec, x_j);
+        __m512d dy_j = _mm512_sub_pd(*y_vec, y_j);
+        __m512d dz_j = _mm512_sub_pd(*z_vec, z_j);
+        __m512d prefact = gravity_prefactor_avx512_one(dx_j, dy_j, dz_j);
+
+        // 0123 4567
+        // 5674 2301
+        __m512d prefact1 = _mm512_mul_pd(prefact, m_j);
+        *vx_vec = _mm512_fnmadd_pd(prefact1, dx_j, *vx_vec);
+        *vy_vec = _mm512_fnmadd_pd(prefact1, dy_j, *vy_vec);
+        *vz_vec = _mm512_fnmadd_pd(prefact1, dz_j, *vz_vec);
+
+        dx_j = _mm512_permutex_pd(dx_j, _MM_PERM_CBAD); // within 256
+        dy_j = _mm512_permutex_pd(dy_j, _MM_PERM_CBAD);
+        dz_j = _mm512_permutex_pd(dz_j, _MM_PERM_CBAD);
+        prefact = _mm512_permutex_pd(prefact, _MM_PERM_CBAD);
+        m_j_01234567 = _mm512_permutex_pd(m_j_01234567, _MM_PERM_CBAD);
+
+        // 4567 1230
+        // 3012 7456
+        prefact = _mm512_mul_pd(prefact, m_j_01234567);
+        dvx = _mm512_fmadd_pd(prefact, dx_j, dvx);
+        dvy = _mm512_fmadd_pd(prefact, dy_j, dvy);
+        dvz = _mm512_fmadd_pd(prefact, dz_j, dvz);
+    }
+
+    // //////////////////////////////////////
+    // 256 bit lane crossing for final add
+    // //////////////////////////////////////
+
+    {
+        dvx = _mm512_permutexvar_pd(kConsts->so2, dvx); // across 512
+        dvy = _mm512_permutexvar_pd(kConsts->so2, dvy);
+        dvz = _mm512_permutexvar_pd(kConsts->so2, dvz);
+
+        *vx_vec = _mm512_add_pd(dvx, *vx_vec);
+        *vy_vec = _mm512_add_pd(dvy, *vy_vec);
+        *vz_vec = _mm512_add_pd(dvz, *vz_vec);
+    }
+}
 
 void whfast512_kernel(__m512d *x_vec, __m512d *y_vec, __m512d *z_vec,
                       __m512d *vx_vec, __m512d *vy_vec, __m512d *vz_vec,
@@ -238,14 +442,17 @@ void whfast512_kernel(__m512d *x_vec, __m512d *y_vec, __m512d *z_vec,
     // We first do a half drift step
     whfast512_drift_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, com, dt/2.);
 
-    // Now we enter main loop
+        // Now we enter main loop
     for (int i = 0; i < Nint - 1; i++)
     {
-        // do jump step. this needs to be split when we add gr
-        whfast512_jump_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt);
+        // do first half-jump step
+        whfast512_jump_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt / 2.);
 
         // do interaction step
-        // whfast512_interaction_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt);
+        whfast512_interaction_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt);
+
+        // do second half-jump step
+        whfast512_jump_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, dt / 2.);
 
         // do drift step
         whfast512_drift_step(x_vec, y_vec, z_vec, vx_vec, vy_vec, vz_vec, m_vec, com, dt);
